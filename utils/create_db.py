66856f371d89ce069db4b667f042d8260b400e2b
keiasz <choi.yosep@gmail.com>
2017-03-11 22:30:13 +0800

    fixed untracked files

--- utils/create_db.py
+++
@@ -1,6 +0,0 @@
-import tables as tb
-
-FILTERS = tb.Filters(complib='blosc', complevel=9)
-h5file = tb.open_file("../data/market.hdf5", mode="a", title="Market Data Collection",
-                      filters=FILTERS)
-
--- utils/getdata_test.py
+++
@@ -1,6 +0,0 @@
-from DataStructure import density, OHLC
-
-
-#if __name__ == "__main__":
-print(density)
-print(OHLC)
